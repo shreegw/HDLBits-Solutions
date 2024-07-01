9b3a3d8609966e93ef07b52991ed97b79edb141e
Shree Ganesh Wooradi <ganeshprime5@gmail.com>
2024-06-30 20:08:13 -0700

    Create Always if.v

---
+++ Verilog Language/Procedures/Always if.v
@@ -0,0 +1,17 @@
+module top_module(
+    input a,
+    input b,
+    input sel_b1,
+    input sel_b2,
+    output wire out_assign,
+    output reg out_always   ); 
+    
+    assign out_assign = (sel_b1 == 1 && sel_b2 == 1)? b : a;
+    
+    always @(*) begin 
+        if (sel_b1==1 && sel_b2 == 1)
+            out_always = b;
+        else 
+            out_always = a;
+    end
+endmodule
