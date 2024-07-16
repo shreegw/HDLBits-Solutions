1a9db4b0cb92978ea4e9e62b2a34c7e60b912f52
Shree Ganesh Wooradi <ganeshprime5@gmail.com>
2024-07-16 12:54:27 -0700

    Create Circuit from Truth Table.v

---
+++ Circuits/Sequential Logic/Latches and Flip Flops/Circuit from Truth Table.v
@@ -0,0 +1,15 @@
+module top_module (
+    input clk,
+    input j,
+    input k,
+    output Q); 
+
+    always @ (posedge clk) begin 
+        case({j,k}) 
+        0: Q <= Q;
+        1: Q <= 0;
+        2: Q <= 1;
+        3: Q <= ~Q;            
+        endcase    
+    end
+endmodule
