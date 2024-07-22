ef43619423f09c3c3be118cd3f211806203f618c
Shree Ganesh Wooradi <ganeshprime5@gmail.com>
2024-07-22 11:55:22 -0700

    Create Count10.v

---
+++ Circuits/Sequential Logic/Counters/Count10.v
@@ -0,0 +1,15 @@
+module top_module (
+    input clk,
+    input reset,      // Synchronous active-high reset
+    output [3:0] q);
+    
+    always @(posedge clk) begin 
+        if(reset)
+            q <= 0;
+        else if(q==4'b1001)
+            q <= 0;
+        else
+            q <= q + 1'b1;       
+    end
+
+endmodule
